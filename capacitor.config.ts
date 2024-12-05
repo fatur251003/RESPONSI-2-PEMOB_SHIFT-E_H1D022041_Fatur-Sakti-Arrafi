a8422b474882e5d208c7fc41526b020304cfa712
fatur251003 <minaritwice2@gmail.com>
2024-12-05 10:51:50 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'responsi_pemob_e',
+  webDir: 'www'
+};
+
+export default config;
